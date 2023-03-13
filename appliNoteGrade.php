ad6f8d29a029d80e525c98735820ac22fd47f627
cinopepito <83885361+cinopepito@users.noreply.github.com>
2023-03-13 15:25:15 +0100

    third commit

---
+++ appliNoteGrade.php
@@ -0,0 +1,17 @@
+<?php
+$note = readline('enter your note please student');
+if($note==10){
+echo 'vous avez D comme grade';    
+}else if($note>10 && $note<=12){
+    echo'vous avez C comme grade';
+}else if($note>12 && $note<=14){
+    echo'vous avez B comme grade';
+}else if($note>14 && $note<=16){
+    echo'vous avez A comme grade';
+}else if($note>16 && $note<=18){
+    echo'vous avez A+ comme grade';
+}
+else{
+    echo'dommage vous n\'avez pas la moyenne';
+}
+?>
---
+++ switch.php
@@ -0,0 +1,20 @@
+<?php
+$color = 'green';
+    switch($color){
+        case'red':
+            echo 'ma couleur est rouge';
+        break;
+        case'green':
+            echo 'ma couleur est verte';
+        break;
+        case'blue':
+            echo 'ma couleur est bleu';
+        break;
+        case'white':
+            echo 'ma couleur est blanche';
+        break;
+        default:
+        echo'enter this colour';
+    }
+
+?>
