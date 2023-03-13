e849fc576700b492dc766783a9ec12678608127e
cinopepito <83885361+cinopepito@users.noreply.github.com>
2023-03-13 16:42:08 +0100

    fourth commit

---
+++ Boucle.php
@@ -0,0 +1,9 @@
+//while loop
+<?php
+$chiffre = null;
+while($chiffre !== 10){
+    $chiffre = (int)readline('Entrer une valeur');
+}
+echo'vous avez gagne'
+?>
+//for locale_compose
---
+++ game.php
@@ -0,0 +1,7 @@
+<?php
+$chiffre = null;
+while($chiffre !== 10){
+    $chiffre = (int)readline('Entrez votre chiffre');
+}
+echo 'vous avez gagné';
+?>
